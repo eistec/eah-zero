b721606c97f2fe77d66bc6c870a21a275f4e6290
Jens Eliasson <jens.eliasson@gmail.com>
2021-11-28 23:07:21 +0100

    added support for testing the DataManager Proxy service

--- Python/HTTP/SimpleSecureConsumer/client.py
+++ Python/HTTP/SimpleSecureConsumer/client.py
@@ -9,16 +9,28 @@ SR_ADDRESS = config['sraddress']
 SR_PORT = config['srport']
 SYS_NAME = config['systemName']
 
-ca_file = "truststore.pem"
-cert_file = "service_registry.pem"
-key_file = "service_registry.key"
+ca_file = config['trustStore'] #"truststore.pem"
+cert_file = config['certFile'] #"service_registry.pem"
+key_file = config['keyFile'] #"service_registry.key"
 
-url = "https://" + SR_ADDRESS + ":" + str(SR_PORT) + "/serviceregistry"
-cert = (cert_file, key_file)
-r = requests.get(url + "/echo", cert=cert, verify=ca_file)
-if r.status_code == 200:
-  print(r.text)
+if False: # check Service registry
+  url = "https://" + SR_ADDRESS + ":" + str(SR_PORT) + "/serviceregistry"
+  cert = (cert_file, key_file)
+  r = requests.get(url + "/echo", cert=cert, verify=ca_file)
+  if r.status_code == 200:
+    print(r.text)
+  else:
+    print('Request failed!\n')
+
+# use the DataManager instead
 else:
-  print('Request failed!\n')
+  url = "https://" + SR_ADDRESS + ":" + str(8461) + "/datamanager/proxy"
+  cert = (cert_file, key_file)
+  r = requests.get(url + "/service_registry.testcloud2.aitia.arrowhead.eu/test", cert=cert, verify=ca_file)
+  if r.status_code == 200:
+    print(r.text)
+  else:
+    print('Request failed! with ' + str(r.status_code)  + '\n')
+    print(r.text)
 
   
