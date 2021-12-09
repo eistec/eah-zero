4314a8508cfb8c1a8f408813e00a9ca2f33d90b5
Jens Eliasson <jens.eliasson@gmail.com>
2021-12-09 21:38:41 +0100

    cleanup

--- NodeJS/DataManagerClient/testclient.js
+++ NodeJS/DataManagerClient/testclient.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 doGet = true
-outMsg = '[{"bn":"tempservice","bt": '+(Math.floor(new Date().getTime() / 1000))+'},{"n":"3303/0/5700","v":-12.4},{"n":"3304/0/5700","v":14.0,"t":0.0}]'
+outMsg = '[{"bn":"tempservice","bt": '+(Math.floor(new Date().getTime() / 1000))+'},{"n":"3303/0/5700","v":-2.4},{"n":"3304/0/5700","v":24}]'
 var headers = {
     'Content-Type': 'application/json',
     'Content-Length': outMsg.length
@@ -11,7 +11,8 @@ var options = {
     host: '127.0.0.1',
     //path: '/datamanager/historian',
     //path: '/datamanager/historian/mulle-342',
-    path: '/datamanager/historian/mulle-342/tempservice',
+    //path: '/datamanager/historian/mulle-342/tempservice',
+    path: '/datamanager/proxy/mulle-342/tempservice',
     port: 8461,
     method: 'GET'
     //method: 'PUT',
@@ -39,6 +40,7 @@ var options = {
         console.log('Fetching data!')
         http.request(options, callback).end();
     } else {
+        console.log('Storing data!')
         const req = http.request(options, callback);
         req.write(outMsg);
         req.end();
